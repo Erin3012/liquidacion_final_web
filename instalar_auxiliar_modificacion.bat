8fc178c145b8fff65ec7a6eff6b72d4b8f9e092b
Erin3012 <ericxxnn20@gmail.com>
2026-07-29 12:11:47 -0400

    Corregir instalador del protocolo local

--- instalar_auxiliar_modificacion.bat
+++ instalar_auxiliar_modificacion.bat
@@ -5,7 +5,7 @@ if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 copy /Y "%~dp0modificacion_helper.ps1" "%INSTALL_DIR%\modificacion_helper.ps1" >nul
 reg add "HKCU\Software\Classes\liquidacion" /ve /d "URL:Liquidacion Web" /f >nul
 reg add "HKCU\Software\Classes\liquidacion" /v "URL Protocol" /d "" /f >nul
-reg add "HKCU\Software\Classes\liquidacion\shell\open\command" /ve /d "powershell.exe -NoProfile -ExecutionPolicy Bypass -File ^"%INSTALL_DIR%\modificacion_helper.ps1^" ^"%%1^"" /f >nul
+reg add "HKCU\Software\Classes\liquidacion\shell\open\command" /ve /d "powershell.exe -NoProfile -ExecutionPolicy Bypass -File %INSTALL_DIR%\modificacion_helper.ps1 %%1" /f >nul
 echo.
 echo Auxiliar instalado correctamente.
 echo Ya puede cerrar esta ventana.
